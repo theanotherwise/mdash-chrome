794bf73c93c0baa082fd5a453bc4c6361d5734f2
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2026-02-21 19:16:01 +0100

    feat: new features implemented

--- pack.sh
+++ pack.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-EXT_VERSION=`cat manifest.json  | jq -r ".version"`
+EXT_VERSION=$(cat manifest.json | jq -r ".version")
+OUT="build/mdash-chrome-${EXT_VERSION}.zip"
 
-zip -r ../mdash-chrome-${EXT_VERSION}.zip * -x "**/.git/**" "**/.DS_Store" "**/node_modules/**" -x "./pack.sh"  -x "./icons/**"
+zip -r "$OUT" * \
+    -x "build/*" \
+    -x "**/.git/**" \
+    -x "**/.DS_Store" \
+    -x "**/node_modules/**" \
+    -x "pack.sh" \
+    -x "icons/*" \
+    -x "AGENTS.md" \
+    -x "STORE_DESCRIPTION.txt" \
+    -x ".gitignore"
+
+echo "Packed: $OUT"
